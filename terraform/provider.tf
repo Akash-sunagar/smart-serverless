21992f38d76aa4088eb51aea0cb1c17799e54f04
root <root@ip-172-31-47-185.ap-south-1.compute.internal>
2026-02-24 07:09:52 +0000

    Remove backend configuration

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,12 +1,3 @@
 provider "aws" {
   region = "ap-south-1"
 }
-
-terraform {
-  backend "s3" {
-    bucket         = "your-terraform-state-bucket"
-    key            = "smart-serverless/terraform.tfstate"
-    region         = "ap-south-1"
-    encrypt        = true
-  }
-}
